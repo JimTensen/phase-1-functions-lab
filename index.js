dc356858c405ec57430035a661ec324b503e5045
Timothy Jensen <timothy.jensen.13@gmail.com>
2023-02-07 00:26:23 -0500

    first attempt functions lab

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(startBlock) {
+    return Math.abs(startBlock - 42);
+}
+
+function distanceFromHqInFeet(startBlock) {
+    return distanceFromHqInBlocks(startBlock) * 264
+}
+
+function distanceTravelledInFeet(streetStart, streetEnd) {
+    const numBlocks = Math.abs(streetStart - streetEnd);
+    return numBlocks * 264;
+}
+
+function calculatesFarePrice(streetStart, streetEnd) {
+    const numBlocks = Math.abs(streetStart - streetEnd);
+    const trip = numBlocks * 264;
+    let fare;
+    
+    if (trip <= 400) {
+        fare = 0;
+    } else if (trip <= 2000) {
+        fare = (trip-400) * 0.02;
+    } else if (trip <= 2500) {
+        fare = 25;
+    } else if (trip > 2500) {
+        fare = "cannot travel that far"
+    }
+    return fare;
+}
+
